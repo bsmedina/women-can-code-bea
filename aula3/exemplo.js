dc880d255d66fe03e299b16e01c22a7bef8acbfc
Beatriz Sampaio Medina <bee.a2@hotmail.com>
2021-11-10 20:43:46 -0300

    exercicio aula 3

---
+++ aula3/exemplo.js
@@ -0,0 +1,11 @@
+const nome = "Beatriz Medina";
+const idade = "25";
+const ehMaiorDeIdade = true;
+
+console.log(nome);
+console.log(idade);
+console.log(ehMaiorDeIdade);
+
+console.log(nome, idade, ehMaiorDeIdade);
+console.log(nome + idade + ehMaiorDeIdade);
+console.log("Meu nome é " + nome + " e tenho " + idade + " anos");
