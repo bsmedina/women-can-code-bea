c0b18396adacb4e1ac1556c2dcf4f971c190014f
Beatriz Sampaio Medina <bee.a2@hotmail.com>
2021-11-11 17:39:14 -0300

    exercicios parte 2 e 3

---
+++ aula3/exercicio5.js
@@ -0,0 +1,11 @@
+// Crie uma variável para armazenar o resultado de cada das operações e imprima o resultado delas.
+
+const soma = 5 + 3;
+const subtracao = 135 - 29;
+const multiplicacao = 4 * 8;
+const divisao = 225 / 5;
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
---
+++ aula3/exercicio8.js
@@ -0,0 +1,23 @@
+//Crie variáveis para cada uma das comparações e imprima o resultado delas
+
+const a = (5 > 1);
+console.log(a);
+
+const b =  (2000000 < 3000000000);
+console.log(b);
+
+const c = ("Banana" == "Banana");
+console.log(c);
+
+const d = ("50" === 50);
+console.log(d);
+
+const e = ("Carro" != "bicicleta");
+console.log(e);
+
+const f = (50 > 27);
+console.log(f);
+
+const g = (975 <= 1200);
+console.log(g);
+
