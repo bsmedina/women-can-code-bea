aa12151cb5c9c50078c512673c267a364c620a07
Beatriz Sampaio Medina <bee.a2@hotmail.com>
2021-11-11 22:01:51 -0300

    anotacoes aula4

---
+++ aula4/exemplo.js
@@ -0,0 +1,32 @@
+const meuCarro = {
+    modelo: "corolla",
+    cor: "cinza",
+    ano: 2019,
+    ehNovo: true
+}
+
+meuCarro.temSeguro = true; // add a propriedade temSeguro no obj
+
+delete meuCarro.cor; // deletar a propriedade de um obj
+
+/*
+***ARRAY***
+*/
+
+const meuArrayStrings = ["Beatriz", "Medina", "São Paulo"];
+
+const meuArrayNumeros = [10, 20, 30, 100, 765, 10];
+
+const meuArrayBoleanos = [true, true, false];
+
+meuArrayObjetos = [
+    {
+        nome: "Beatriz",
+        idade: 25
+    },
+    {
+        profissão: estudante
+    }
+]
+
+const meuArrayVariosTipos = [ "Abacaxi", 700, true, {eletronico: "nootebook"}];
